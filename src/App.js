03c24073650b77789f8c61337f74915f1e5e4c72
Keila Amada <keila.amadap@hotmail.com>
2023-01-05 17:09:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import api from "./services/api";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  // requisição com promise
+
+  // api
+  //   .get("https://jsonplaceholder.typicode.com/todos/")
+  //   .then((response) => setData(response));
+
+  const getApi = async () => {
+    let response = await api.get("https://jsonplaceholder.typicode.com/photos");
+    setData(response.data);
+  };
+
+  const getUsers = async () => {
+    let response = await api.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+    getApi();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data
+        .filter((predicate) => predicate.albumId === 1)
+        .map((photo) => (
+          <div style={{ backgroundColor: "red", borderRadius: "10px" }}>
+            <h1>{photo.id}</h1>
+            <h2> {photo.title} </h2>
+            <h3> {photo.url} </h3>
+            <img src={photo.thumbnailUrl} alt="Imagem nao encontrada" />
+          </div>
+        ))}
+      {data
+        .filter((predicate) => predicate.albumId === 2)
+        .map((photo) => (
+          <div style={{ backgroundColor: "green", borderRadius: "10px" }}>
+            <h1>{photo.id}</h1>
+            <h2> {photo.title} </h2>
+            <h3> {photo.url} </h3>
+            <img src={photo.thumbnailUrl} alt="Imagem nao encontrada" />
+          </div>
+        ))}
+      {users
+        .filter((predicate) => predicate.id <= 5)
+        .map((user) => (
+          <div>
+            <h1>{user.name}</h1>
+            <h2> {user.email} </h2>
+            <h3> {user.adress} </h3>
+            <div>{user.city}</div>
+          </div>
+        ))}
     </div>
   );
 }
-
-export default App;
